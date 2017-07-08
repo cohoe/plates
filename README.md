e4d24dd205f493bdb422298c678b4f8088e04bb5
Grant Cohoe <grant@grantcohoe.com>
2017-07-08 07:18:24 -0400

    fixed bug with notification

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Your #1 source for garbage plate reviews in the greater-Rochester area and beyon
 Notes
 -----
 S3 Bucket: http://platesofrochester.s3-website-us-east-1.amazonaws.com/
+
+ToDo
+----
+* Error handler
+* Proper static frontend
+* Data model handling or objects
+* Date submitted sorter
--- platesvc/handler.py
+++ platesvc/handler.py
@@ -7,7 +7,13 @@ def notifypending(event, context):
     """
     arn = "arn:aws:sns:us-east-1:460782256770:plates-PendingReviews"
     url_prefix="http://platesofrochester.s3-website-us-east-1.amazonaws.com/approval.html?id="
+
     #message = {"event": event}
+
+    operation = event['Records'][0]['eventName']
+
+    if "INSERT" not in operation:
+        return
 
     review_id = event['Records'][0]['dynamodb']['Keys']['id']['S']
     message = "Review %s%s has been submitted for review." % (url_prefix, review_id)
