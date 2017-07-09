7b944d63afa029b6d19c4f41ed3055dbd24b1ca8
Grant Cohoe <grant@grantcohoe.com>
2017-07-09 18:55:33 -0400

    filled out the rest of the review form

--- web/assets/js/plates.js
+++ web/assets/js/plates.js
@@ -45,6 +45,21 @@ $(function() {
         console.log("Submitting review");
         endpoint = api_prefix + "/review"
 
+        missingFields = ""
+
+        $('#reviewForm').find('input').each(function() {
+            if($(this).prop('required') != false) {
+                missingFields += $(this).prop('name')
+                missingFields += ", "
+            }
+        });
+        
+        if (missingFields != "") {
+            $('#reviewError').html("You are missing required fields: " + missingFields);
+            $('#errorModal').modal('show');
+            return
+        }
+
         $.ajax({
             type: "POST",
             url: endpoint,
@@ -67,5 +82,38 @@ $(function() {
         console.log("End Submit review");
     });
 });
+
+$(function()
+	{
+		// editable stars
+		$('.stars.editable').each(function(i, e)
+			{
+				$(e).mousemove(function(e)
+					{
+						var offs = $(this).offset();
+						var myval = 8 * (Math.round((e.pageX - offs.left) / 8));
+						$('.stars-inner', this).css('width', myval);
+						$(this).next('.stars-label').text(Math.round(myval / 8, 1) / 2);
+					}).mouseout(function(e)
+					{
+						var offs = $(this).offset();
+
+						if ( e.pageX >= offs.left && e.pageY >= offs.top && e.pageX <= offs.left + ($(this).attr('data-size') * 16) && e.pageY <= offs.top + 16 )
+						{
+							// don't do this function if we're still inside the element
+							return;
+						}
+
+						$(this).next('.stars-label').text('');
+						$('.stars-inner', this).css('width', Number($(this).next().next().val()) * 16);
+					}).click(function(e)
+						{
+							var offs = $(this).offset();
+							var myval = 8 * (Math.round((e.pageX - offs.left) / 8));
+							$(this).next().next().val(Math.round(myval / 8, 1) / 2);
+						}).mouseout();
+
+			});
+           });
 
 console.log("Done running Custom Script");
